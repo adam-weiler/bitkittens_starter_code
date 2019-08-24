653fde2286e0c2a789823746d4d71cc783bde49f
adam-weiler <adam.weiler@yahoo.ca>
2019-08-24 13:15:04 -0400

    When button is clicked the 3 divs are populated with cats from the API request.

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -5,9 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const allCatBoxes = document.querySelectorAll('.cat-box');
     const summonKittensButton = document.querySelector('#summon-cats');
 
-    console.log(allCatBoxes);
-    console.log(summonKittensButton);
-
+    // console.log(allCatBoxes);
+    // console.log(summonKittensButton);
 
 
     summonKittensButton.addEventListener('click', () => {
@@ -19,17 +18,26 @@ document.addEventListener("DOMContentLoaded", function() {
             // console.log(response.data);
 
             let catsList = response.data['cats'];
-            console.log(catsList);
-            console.log(catsList[0]['name']);
+            // console.log(catsList);
+            // // console.log(catsList[0]['name']);
+            
+            allCatBoxes.forEach((box, index) => {  // For each value in the allCatBoxes list.
+                box.innerHTML = "";  // Removes previous cat.
+
+                let dataElem = document.createElement('img');
+                dataElem.src = catsList[index]['photo'];
+                dataElem.alt = `Photo of ${catsList[index]['name']}`;
+                
+                box.appendChild(dataElem);
+            })
 
         })
         .catch((error) => {
             console.log('-- Received error.');
-
+            console.log(error);
         })
         .then(() => {
             console.log('-- Request is over.');
-
         });
     });
 });

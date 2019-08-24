a8a47d8f9923e296a334c5a32ae15e9b7965e0c1
adam-weiler <adam.weiler@yahoo.ca>
2019-08-24 12:48:44 -0400

    Making the AJAX request, storing data in catsList variable.

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // const url = "http://bitkittens.herokuapp.com/cats.json";  // API is down.
+    const url = "https://eric-deploy-test.herokuapp.com/cats";  // Eric's API.
+
     const allCatBoxes = document.querySelectorAll('.cat-box');
     const summonKittensButton = document.querySelector('#summon-cats');
 
@@ -10,5 +13,23 @@ document.addEventListener("DOMContentLoaded", function() {
     summonKittensButton.addEventListener('click', () => {
         console.log('-- Clicked the \'Summon kitties!\' button.');
 
+        axios.get(url)
+        .then((response) => {
+            console.log('-- Received response.');
+            // console.log(response.data);
+
+            let catsList = response.data['cats'];
+            console.log(catsList);
+            console.log(catsList[0]['name']);
+
+        })
+        .catch((error) => {
+            console.log('-- Received error.');
+
+        })
+        .then(() => {
+            console.log('-- Request is over.');
+
+        });
     });
 });

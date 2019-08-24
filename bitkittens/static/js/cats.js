5844d7d502ca467773d8d0cfdd3e2542084d1440
adam-weiler <adam.weiler@yahoo.ca>
2019-08-24 12:09:54 -0400

    Adding event listener on summon kitties button.

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const allCatBoxes = document.querySelectorAll('.cat-box');
+    const summonKittensButton = document.querySelector('#summon-cats');
+
+    console.log(allCatBoxes);
+    console.log(summonKittensButton);
+
+
+
+    summonKittensButton.addEventListener('click', () => {
+        console.log('-- Clicked the \'Summon kitties!\' button.');
+
+    });
 });
